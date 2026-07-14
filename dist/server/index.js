75ce33600b2c195ae6927bd71698ee59b3a6b2a8
Pay4att <1718014302@qq.com>
2026-07-14 14:20:51 +0800

    Remove Sites deployment artifacts

--- dist/server/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  async fetch(request, env) {
-    const asset = await env.ASSETS.fetch(request);
-    if (asset.status !== 404) return asset;
-    return env.ASSETS.fetch(new Request(new URL("/", request.url)));
-  },
-};
